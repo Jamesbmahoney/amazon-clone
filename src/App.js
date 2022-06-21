faf33510788b4addea187ae5b68544d85e4e57ad
James Barry <jbm03003@gmail.com>
2022-06-21 19:00:51 -0400

    finished initial Home component, populated with Product component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Header from './Header';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     // BEM
     <div className="app">      
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
-import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket"
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
   return (
@@ -29,11 +29,10 @@ function Header() {
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
-<div className="header__optionBasket">
-    <ShoppingBasketIcon />
-    <span className="header__optionLineTwo header__basketCount">0</span>
-</div>
-
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
       </div>
     </div>
   );
