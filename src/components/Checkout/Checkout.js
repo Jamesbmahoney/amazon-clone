7762a8a963e23aefc4061dbb22f837b3d8ce8413
James Barry <jbm03003@gmail.com>
2022-07-19 18:48:37 -0400

    added a subtotal section and styling

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from "../Subtotal/Subtotal"
 
 function Checkout() {
   return (
@@ -23,7 +24,7 @@ function Checkout() {
       </div>
 
       <div className="checkout__right">
-        <h2>Subtotal here</h2>
+        <Subtotal />        
       </div>
     </div>
   );
