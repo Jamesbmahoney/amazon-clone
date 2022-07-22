041fa74a651a521039a5c3fdc63d4222917a48a6
James Barry <jbm03003@gmail.com>
2022-07-22 14:03:28 -0400

    Added styling to login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,7 +13,25 @@ function Login() {
         />
       </Link>
       <div className="login__container">
+        <h1>Sign In</h1>
 
+        <form>
+          <h5>E-Mail</h5>
+          <input type="text" />
+       
+          <h5>Password</h5>
+          <input type="password" />
+
+          <button className="login__signInButton">Sign In</button>
+        </form>
+
+        <p>
+          By signing-in you agree to AMAZON CLONE's Conditions of Use & Sale, Please
+          see our Privacy Notice, our Cookies Notice and our Internet Based Ads
+          Notice.
+        </p>        
+
+      <button className="login__registerButton">Create your Amazon account</button>        
       </div>
     </div>
   );
